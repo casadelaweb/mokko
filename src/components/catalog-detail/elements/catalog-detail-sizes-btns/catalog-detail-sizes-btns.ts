fcd188f5e59b606b42d23d98bf193c4afcfc6c41
Alex Volkov <a.volkov@3l.ru>
2023-06-11 02:48:21 +0500

    добавлены проверки на пустоту

--- src/components/catalog-detail/elements/catalog-detail-sizes-btns/catalog-detail-sizes-btns.ts
+++ src/components/catalog-detail/elements/catalog-detail-sizes-btns/catalog-detail-sizes-btns.ts
@@ -2,30 +2,34 @@ import toggle from 'src/modules/toggle/toggle'
 
 document.addEventListener('DOMContentLoaded', () => {
   toggle('.catalog-detail-info-block-btn')
-  const {body,} = document
+  const { body, } = document
   const sizeBtnsItems: HTMLElement[] = Array.from(body.querySelectorAll('.catalog-detail-sizes-item'))
   const sizeBtn: HTMLElement = body.querySelector('.catalog-detail-btn-size')
   const catalogDetailSizesInfo: HTMLElement = body.querySelector('.catalog-detail-sizes-info')
   const catalogDetailBtns: HTMLElement = body.querySelector('.catalog-detail-btns')
   const activeClass: string = 'catalog-detail-sizes-item-active'
 
+  document.addEventListener('click', (event) => {
+    const target = event as HTMLElement
+    if (target.closest('.catalog-detail-sizes-item')) {
+      const button = target.closest('.catalog-detail-sizes-item')
 
-  sizeBtnsItems.forEach((btn: HTMLElement) => {
-    console.log(btn)
-    btn.addEventListener(('click'), () => {
       sizeBtnsItems.forEach((element: HTMLElement) => {
         element.classList.remove(activeClass)
       })
-      btn.classList.add(activeClass)
-      const textArr: string[] = btn.textContent.trim().split(' ')
+
+      button.classList.add(activeClass)
+
+      const textArr: string[] = button.textContent.trim().split(' ')
       sizeBtn.children[0].textContent = `${textArr[0]} (${textArr[1]})`
-    })
+    }
   })
 
-  const catalogDetailSizesInfoMob: HTMLElement = catalogDetailSizesInfo.cloneNode()
-  catalogDetailSizesInfoMob.classList.add('catalog-detail-sizes-info-mob')
-  catalogDetailSizesInfoMob.innerHTML = catalogDetailSizesInfo.innerHTML
+  if (catalogDetailSizesInfo) {
+    const catalogDetailSizesInfoMob: HTMLElement = catalogDetailSizesInfo.cloneNode()
+    catalogDetailSizesInfoMob.classList.add('catalog-detail-sizes-info-mob')
+    catalogDetailSizesInfoMob.innerHTML = catalogDetailSizesInfo.innerHTML
 
-  catalogDetailBtns.insertAdjacentHTML('beforebegin', catalogDetailSizesInfoMob.outerHTML)
-
+    catalogDetailBtns.insertAdjacentHTML('beforebegin', catalogDetailSizesInfoMob.outerHTML)
+  }
 })
