061bc90c82f043008d23c625cb10365e514e827a
Alex Volkov <a.volkov@3l.ru>
2023-06-11 00:13:40 +0500

    добавлены анимации для слайдера в каталоге

--- src/components/catalog/catalog.ts
+++ src/components/catalog/catalog.ts
@@ -1,33 +1,84 @@
-import { catalogElements } from 'src/components/catalog/catalog.types'
+import { catalogElements, selectors } from 'src/components/catalog/catalog.types'
 
 export class Catalog {
+  private isDesktop: boolean
+  private selectors: selectors
   private elements: catalogElements
 
+  constructor() {
+    this.selectors = {
+      card: '[data-catalog=card]',
+      slider: '[data-slider=catalog-card]',
+      controls: '[data-catalog-card=controls]',
+      buttons: {
+        prev: '.swiper-button-prev',
+        next: '.swiper-button-next',
+      },
+    }
+    this.isDesktop = false
+  }
+
   public init(): void {
+    this.isDesktop = window.matchMedia('(min-width: 1280px)').matches
     this.elements = this.updateElements()
     this.listen()
   }
 
   private listen(): void {
-    document.addEventListener('click', (event) => {
-      const target = event.target as HTMLElement
+    document.addEventListener('click', this.clickHandler.bind(this))
+    this.updateListeners()
+  }
 
-      if (target.closest('[data-catalog-layout]')) {
-        const button = target.closest('[data-catalog-layout]')
-        const mode = button.getAttribute('data-catalog-layout')
+  private clickHandler(event): void {
+    const target = event.target as HTMLElement
 
-        this.changeLayoutMode(mode)
-      }
+    if (target.closest('[data-catalog-layout]')) {
+      const button = target.closest('[data-catalog-layout]')
+      const mode = button.getAttribute('data-catalog-layout')
 
-      if (target.closest('[data-catalog-card=slider]')) {
-        const card = target.closest('[data-catalog=card]')
-        const controls = card.querySelector('[data-catalog-card=controls]')
-        controls.classList.toggle('active')
-      } else {
-        const controls = document.body.querySelectorAll('[data-catalog-card=controls]')
-        controls.forEach((element) => element.classList.remove('active'))
-      }
+      this.changeLayoutMode(mode)
+    }
+
+    if (target.closest(this.selectors.slider) && !this.isDesktop) {
+      const card = target.closest(this.selectors.card)
+      const controls = card.querySelector(this.selectors.controls)
+      controls.classList.toggle('active')
+    }
+  }
+
+  private updateListeners(): void {
+    const { body, } = document
+
+    const cards: HTMLElement[] = Array.from(body.querySelectorAll(this.selectors.card))
+    cards.forEach((card: HTMLElement) => {
+      card.removeEventListener('mouseenter', this.mouseEnterHandler)
+      card.removeEventListener('mouseleave', this.mouseLeaveHandler)
+
+      card.addEventListener('mouseenter', this.mouseEnterHandler.bind(this))
+      card.addEventListener('mouseleave', this.mouseLeaveHandler.bind(this))
     })
+  }
+
+  private mouseEnterHandler(event): void {
+    const card = event.target as HTMLElement
+    const sizes: HTMLElement = card.querySelector(this.selectors.controls)
+    const buttonPrev: HTMLElement = card.querySelector(this.selectors.buttons.prev)
+    const buttonNext: HTMLElement = card.querySelector(this.selectors.buttons.next)
+
+    sizes.classList.add('active')
+    buttonPrev.classList.add('active')
+    buttonNext.classList.add('active')
+  }
+
+  private mouseLeaveHandler(event): void {
+    const card = event.target as HTMLElement
+    const sizes: HTMLElement = card.querySelector(this.selectors.controls)
+    const buttonPrev: HTMLElement = card.querySelector(this.selectors.buttons.prev)
+    const buttonNext: HTMLElement = card.querySelector(this.selectors.buttons.next)
+
+    sizes.classList.remove('active')
+    buttonPrev.classList.remove('active')
+    buttonNext.classList.remove('active')
   }
 
   private changeLayoutMode(mode: string): void {
--- src/components/catalog/catalog.types.ts
+++ src/components/catalog/catalog.types.ts
@@ -1,3 +1,13 @@
 export interface catalogElements {
   layout: HTMLElement | null
 }
+
+export interface selectors {
+  slider: string,
+  controls: string,
+  card: string,
+  buttons: {
+    prev: string,
+    next: string,
+  },
+}
