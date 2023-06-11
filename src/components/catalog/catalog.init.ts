69222018d3cab90e93a7de3b9561dbcb2066158c
Alex Volkov <a.volkov@3l.ru>
2023-06-11 20:43:59 +0500

    мобильное меню для каталога WIP

--- src/components/catalog/catalog.init.ts
+++ src/components/catalog/catalog.init.ts
@@ -4,12 +4,30 @@ import 'swiper/scss/a11y'
 import { accessibility as accessibilitySettings } from 'src/scripts/swiper-settings'
 import { Catalog } from 'src/components/catalog/catalog'
 
+import * as noUiSlider from 'nouislider'
+import 'nouislider/dist/nouislider.css'
+import './no-ui-slider.scss'
+
 document.addEventListener('DOMContentLoaded', () => {
   const catalog = new Catalog()
   catalog.init()
 
-  const catalogHeader: HTMLElement = document.body.querySelector('[data-catalog=header]')
+  const { body, } = document
+
+  const catalogHeader: HTMLElement = body.querySelector('[data-catalog=header-desktop]')
   if (catalogHeader) catalogHeader.style.overflow = 'unset'
+
+  noUiSlider.create(body.querySelector('#prices'), {
+    start: [
+      0,
+      100,
+    ],
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 100,
+    },
+  })
 
   document.addEventListener('click', (event) => {
     const target = event.target as HTMLElement
