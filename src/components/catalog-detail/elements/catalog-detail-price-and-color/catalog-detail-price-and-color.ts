eef68720f6a656485575233d2b47897f450a1dab
Krel <molchanov_i_v@mail.ru>
2023-06-14 21:45:53 +0500

    поправил ошибку со слайдером

--- src/components/catalog-detail/elements/catalog-detail-price-and-color/catalog-detail-price-and-color.ts
+++ src/components/catalog-detail/elements/catalog-detail-price-and-color/catalog-detail-price-and-color.ts
@@ -1,19 +1,5 @@
-import Swiper, { Pagination } from 'swiper'
+import Swiper, {Pagination} from 'swiper'
 import 'swiper/scss'
 // import 'swiper/scss/pagination'
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  new Swiper('.catalog-detail-gallery-slider', {
-    enabled: true,
-    slidesPerView: 1,
-    modules: [ Pagination, ],
-
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
-    breakpoints: { 768: { enabled: false, }, },
-  })
-})
