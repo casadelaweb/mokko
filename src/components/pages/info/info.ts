5ede3446348fa8903b9e9a2533ee4db392694de0
Alex Volkov <a.volkov@3l.ru>
2023-09-17 18:12:32 +0500

    страница Мои заказы/Заказ

--- src/components/pages/info/info.ts
+++ src/components/pages/info/info.ts
@@ -1,30 +1,78 @@
+import { globalScrollController } from 'src/scripts/global-scroll-controller'
+
+class InfoAside {
+  private header: HTMLElement
+  private nav: HTMLElement
+  private button: HTMLElement
+  private list: HTMLElement
+  private overlay: HTMLElement
+  private selectors: {
+    header: string,
+    nav: string,
+    button: string,
+    list: string,
+    overlay: string,
+  }
+
+  constructor() {
+    this.selectors = {
+      header: '.info-nav-header',
+      nav: '.info-nav',
+      button: '.info-nav-button',
+      list: '.info-nav-list',
+      overlay: '.info-nav-overlay',
+    }
+  }
+
+  public init(): void {
+    this.updateElements()
+  }
+
+  public activate(): void {
+    this.header.classList.add('active')
+    this.nav.classList.add('active')
+    this.button.classList.add('active')
+    this.list.classList.add('active')
+    this.overlay.classList.add('active')
+  }
+
+  public deactivate(): void {
+    this.header.classList.remove('active')
+    this.nav.classList.remove('active')
+    this.button.classList.remove('active')
+    this.list.classList.remove('active')
+    this.overlay.classList.remove('active')
+  }
+
+  private updateElements(): void {
+    const body = document.body as HTMLElement
+
+    this.header = body.querySelector(this.selectors.header)
+    this.nav = body.querySelector(this.selectors.nav)
+    this.button = body.querySelector(this.selectors.button)
+    this.list = body.querySelector(this.selectors.list)
+    this.overlay = body.querySelector(this.selectors.overlay)
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const { body, } = document
 
+  const aside = new InfoAside()
+  aside.init()
+
   if (body.querySelector('.info-nav')) {
-    let isInfoNavActive = false
+    const isInfoNavActive = false
+
     document.addEventListener('click', (event: MouseEvent) => {
       const target = event.target as HTMLElement
 
       if (target.closest('.info-nav-header')) {
-        const header = target.closest('.info-nav-header')
-        const nav = header.closest('.info-nav')
-        const button = nav.querySelector('.info-nav-button')
-        const list = nav.querySelector('.info-nav-list')
-        nav.classList.toggle('active')
-        list.classList.toggle('active')
-        button.classList.toggle('active')
-
-        isInfoNavActive = !isInfoNavActive
+        aside.activate()
       } else if (isInfoNavActive && !target.closest('.info-nav')) {
-        const nav = body.querySelector('.info-nav')
-        // const header = nav.querySelector('.info-nav-header')
-        const button = nav.querySelector('.info-nav-button')
-        const list = nav.querySelector('.info-nav-list')
-
-        nav.classList.remove('active')
-        list.classList.toggle('active')
-        button.classList.toggle('active')
+        aside.deactivate()
+      } else if (target.closest('.info-nav-overlay')) {
+        aside.deactivate()
       }
     })
   }
